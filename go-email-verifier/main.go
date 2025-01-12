7901e87e9509fc0c908ed1c25ebe65b0972d49eb
Muhammad Saim <muhammadsaim494@gmail.com>
2025-01-12 09:50:47 +0500

    fetch the MX records

--- go-email-verifier/main.go
+++ go-email-verifier/main.go
@@ -4,12 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Domain -- HasMX -- HasSPF -- SPRecord -- HasDMARC -- DmarcRecord\n")
+	fmt.Printf("Domain -- HasMX -- HasSPF -- SPFRecord -- HasDMARC -- DmarcRecord\n")
 
 	for scanner.Scan() {
 		CheckDomain(scanner.Text())
@@ -22,4 +23,19 @@ func main() {
 
 // TODO: CheckDomain if valid or invalid
 func CheckDomain(domain string) {
+	// init the variables
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, DmarcRecord string
+
+	// lookup for MX records
+	mxRecords, err := net.LookupMX(domain)
+	// check weather there is any error
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	// true the flag if there is any records
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
 }
